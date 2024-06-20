71f1ac0d719bd6fd623cd216f184e0e7cc426d42
yongkhengs <yongkhengs@gmail.com>
2024-06-20 19:00:07 +0800

    chore: file rename

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
-import Router from "./routes";
+import Router from "./Routes";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
